2ebeb1e7fed654b6d859ac5c705f843076553a4d
shinshARK <137866950+shinshARK@users.noreply.github.com>
2025-08-31 13:33:32 +0700

    feat: Complete ch. 1 l.5 & l.6 prepare for multiturn chat and flags

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 
@@ -14,20 +15,44 @@ def main():
         print('\nUsage: python main.py "your prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-    user_prompt = " ".join(args)
+
+    flags, user_prompt = parse_args(args)
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, flags)   
+
+def parse_args(args):
+
+    flags = {}
+    prompt = []
+    for arg in args:
+        if arg.startswith('--'): 
+            flags[arg[2:]] = True
+        else:
+            prompt.append(arg)
+    prompt = ' '.join(prompt)
+    return flags, prompt
+
+def generate_content(client, messages, flags):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=user_prompt,
+        contents=messages,
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if flags.get('verbose', False):
+        print("User prompt:", messages[0].parts[0].text)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
 
 
 if __name__ == "__main__":
     main()
+
+
